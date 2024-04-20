a4e6f7fe5e86cf227f7dc66759bc43a0ebc50c34
Seven Du <whine.whitish.0u@icloud.com>
2024-04-20 15:38:50 +0800

    chore(store): add store classes comments

--- lib/src/store/types.dart
+++ lib/src/store/types.dart
@@ -1,18 +1,26 @@
+/// Store subscriber.
 typedef Subscriber<T> = void Function(T value);
+
+/// Store ubsubscriber.
 typedef Unsubscriber = void Function();
+
+/// Store uodater.
 typedef Updater<T> = T Function(T value);
+
+/// Store notifier.
 typedef StartStopNotifier<T> = Unsubscriber? Function(
     ({
       Subscriber<T> set,
-      T Function() get,
       void Function(Updater<T> updater) update,
     }));
 
+/// Readable store.
 abstract interface class Readable<T> {
   /// Subscribe on value changes
   Unsubscriber subscribe(Subscriber<T> subscriber);
 }
 
+/// Writeable store.
 abstract interface class Writeable<T> implements Readable<T> {
   /// Set value and inform subscribers
   void set(T value);
