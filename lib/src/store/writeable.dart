771a0e904281889c960a96e7c8b9e2b115617dba
Seven Du <whine.whitish.0u@icloud.com>
2024-04-20 15:39:07 +0800

    chore(store): add writeable comments

--- lib/src/store/writeable.dart
+++ lib/src/store/writeable.dart
@@ -27,7 +27,7 @@ class _Writeable<T> implements Writeable<T>, CurrentSource<T> {
     subscribers.add(subscriber);
 
     if (subscribers.length == 1) {
-      final props = (set: set, update: update, get: () => source);
+      final props = (set: set, update: update);
       stop = start?.call(props);
     }
 
@@ -45,5 +45,24 @@ class _Writeable<T> implements Writeable<T>, CurrentSource<T> {
   void update(Updater<T> updater) => set(updater(source));
 }
 
+/// Function that creates a store which has values that can be set from
+/// 'outside' components. It gets created as an record with additional `set` and
+/// `update` methods.
+///
+/// - `set`: is a method that takes one argument which is the value to be set.
+/// The store value gets set to the value of the argument if the store value is
+/// not already equal to it.
+/// - `update`: is a method that takes one argument which is a callback. The
+/// callback takes the existing store value as its argument and returns the new
+/// value to be set to the store.
+///
+/// ```dart
+/// final count = writeable(0);
+///
+/// count.subscribe((value) => print(value));
+///
+/// count.set(1); // Print 1
+/// count.update((value) => value + 1); // Print 2
+/// ```
 Writeable<T> writeable<T>(T value, [StartStopNotifier<T>? start]) =>
     _Writeable(value, start);
