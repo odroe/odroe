821ec06eba5092011c3ee97ea3e53943121a2ac2
Seven Du <whine.whitish.0u@icloud.com>
2024-04-20 15:42:26 +0800

    chore(widget): add class-style widget comments

--- lib/widget.dart
+++ lib/widget.dart
@@ -5,6 +5,24 @@ import 'package:flutter/widgets.dart';
 import 'src/element.dart';
 import 'src/runes/context.dart';
 
+/// Class-style Widget when Setup runes base widget.
+///
+/// ```dart
+/// class Counter extends SetupWidget {
+///   const Counter({super.key});
+///
+///   Widget build(BuildContext context) {
+///     final count = $state(0);
+///
+///     void increment() => count.set(count.get() + 1);
+///
+///     return TextButton(
+///       onPressed: increment,
+///       child: Text('Count: ${count.get()}),
+///     );
+///   }
+/// }
+/// ```
 abstract class SetupWidget extends Widget {
   const SetupWidget({super.key});
 
