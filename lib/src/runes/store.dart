49aa16ec2ce50e4a4a0f40539a02306ecb169ae7
Seven Du <whine.whitish.0u@icloud.com>
2024-04-20 14:33:00 +0800

    chore: add `$store` rune comments

--- lib/src/runes/store.dart
+++ lib/src/runes/store.dart
@@ -1,6 +1,25 @@
 import 'package:odroe/odroe.dart';
 import 'package:odroe/store.dart';
 
+/// Create a rune linking a store.
+///
+/// ```dart
+/// final counterStore = writeable(0);
+///
+/// Widget example() => setup(() {
+///   final count = $store(counterStore);
+///
+///   void plusOne() => counterStore.update((value) => value + 1);
+///
+///   return TextButton(
+///     onPressed: plusOne,
+///     child: Text('Count: $count'),
+///   );
+/// });
+/// ```
+///
+/// [$store] Returns the value of the current Store, and notifies the widget to
+/// rebuild when the value of the Store changes.
 T $store<T>(Readable<T> store) {
   final ref = $state(get(store));
 
