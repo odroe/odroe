abce68603233523f9db6e663c3db07f6d2a04c25
Seven Du <whine.whitish.0u@icloud.com>
2024-04-20 15:38:19 +0800

    chore(store): add `readable` comments

--- lib/src/store/readable.dart
+++ lib/src/store/readable.dart
@@ -22,5 +22,20 @@ class _Readable<T>
   }
 }
 
+/// Creates a store whose value cannot be set from 'outside', the first
+/// argument is the store's initial value, and the second argument to
+/// [readable] is the same as the second argument to [writable].
+///
+/// ```dart
+/// final time = readable(new DateTime(), (actions) {
+///   actions.set(new DateTime());
+///
+///   final timer = Timer.periodic(const Duration(seconds: 1), (_) {
+///     actions.set(new DateTime());
+///   });
+///
+///   return timer.cancel;
+/// });
+/// ```
 Readable<T> readable<T>(T value, [StartStopNotifier<T>? start]) =>
     _Readable(writeable(value, start));
