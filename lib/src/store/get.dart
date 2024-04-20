343e040210f701df65b9fc666105024b55c82355
Seven Du <whine.whitish.0u@icloud.com>
2024-04-20 15:38:04 +0800

    chore(store): add `get` comments

--- lib/src/store/get.dart
+++ lib/src/store/get.dart
@@ -1,6 +1,7 @@
 import 'source.dart';
 import 'types.dart';
 
+/// Returns the current value of the store.
 T get<T>(Readable<T> store) {
   assert(store is CurrentSource<T>);
   return (store as CurrentSource<T>).source;
