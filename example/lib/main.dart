702c028a4aa8cebfdb68b0fcb4df97e59304394b
Seven Du <whine.whitish.0u@icloud.com>
2024-04-22 00:39:20 +0800

    chore: example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,13 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:odroe/odroe.dart';
 
 import 'routes.dart';
-import 'theme.dart';
 
 Widget app() => setup(() {
       return MaterialApp(
-        theme: $store(lightThemeStore),
-        darkTheme: $store(darkThemeStore),
-        themeMode: $store(modeThemeStore),
         title: 'Odroe Examples',
         initialRoute: initialRoute,
         routes: routes,
