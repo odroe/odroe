b26676c2065df252d8db2eb80dd0ce5e5dbc775e
Seven Du <whine.whitish.0u@icloud.com>
2024-04-22 00:40:18 +0800

    chore(example): cleanup

--- example/lib/theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:odroe/store.dart';
-
-final lightThemeStore = writeable(ThemeData());
-final darkThemeStore = writeable(ThemeData.dark());
-final modeThemeStore = writeable(ThemeMode.system);
