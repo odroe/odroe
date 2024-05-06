221c18e6e6fd0c4f70768e0dd19039f5f149b1f1
Seven Du <whine.whitish.0u@icloud.com>
2024-05-06 22:27:51 +0800

    chore: *

--- sites/odroe.com/svelte.config.js
+++ sites/odroe.com/svelte.config.js
@@ -1,5 +1,5 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,8 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      runtime: 'edge',
+      edge: false,
+      split: false,
     }),
 
     paths: {
