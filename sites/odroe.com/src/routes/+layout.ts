42639a8ee24685ae187007febaddc104d2dabdf6
Seven Du <whine.whitish.0u@icloud.com>
2024-05-07 02:04:43 +0800

    chore: remove vercel analytics

--- sites/odroe.com/src/routes/+layout.ts
+++ sites/odroe.com/src/routes/+layout.ts
@@ -1,8 +1,3 @@
 import '../app.css';
 
-import { dev } from '$app/environment';
-import { inject } from '@vercel/analytics';
-
-inject({ mode: dev ? 'development' : 'production' });
-
 export const prerender = true;
