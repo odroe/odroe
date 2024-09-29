c77f16dc7135abcd557292efd7e6baa68ea4e884
Seven Du <shiweidu@outlook.com>
2024-09-29 10:23:49 +0800

    chore: WIP

--- pub/oref/lib/src/provide_inject.dart
+++ pub/oref/lib/src/provide_inject.dart
@@ -13,6 +13,16 @@ class _Store {
 final _refs = Expando<_Store>();
 final _targets = Expando<Map<Symbol, WeakReference<Element>>>();
 
+/// Provides a value associated with a key in the given BuildContext.
+///
+/// This function is used to store a value of type [T] associated with a [key]
+/// in the context of a specific widget. If the value has changed, it triggers
+/// a rebuild of dependent widgets.
+///
+/// Parameters:
+/// - [context]: The BuildContext in which to provide the value.
+/// - [key]: A Symbol used as the identifier for the provided value.
+/// - [value]: The value to be provided.
 void provide<T>(BuildContext context, Symbol key, T value) {
   var store = _refs[context] ??= _Store(context.widget);
   if (!Widget.canUpdate(store.widget, context.widget)) {
@@ -25,6 +35,18 @@ void provide<T>(BuildContext context, Symbol key, T value) {
   }
 }
 
+/// Injects a value associated with a key from the widget tree.
+///
+/// This function attempts to retrieve a value of type [T] associated with the
+/// given [key]. It searches the widget tree upwards, starting from the given
+/// [context], until it finds a matching value or reaches the root.
+///
+/// Parameters:
+/// - [context]: The BuildContext from which to start the search.
+/// - [key]: A Symbol used as the identifier for the value to be injected.
+///
+/// Returns:
+/// The injected value of type [T], or null if not found.
 T? inject<T>(BuildContext context, Symbol key) {
   final (v, f) = _lookupValueAndTrack<T>(context, key);
   if (f) return v;
