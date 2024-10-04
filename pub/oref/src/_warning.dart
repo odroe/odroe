68e9b467ca71311c10d8812e8c3be32b0b8e1ec0
Seven Du <shiweidu@outlook.com>
2024-10-04 10:35:00 +0800

    chore: WIP

--- pub/oref/src/_warning.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:developer';
-
-void warn(String message) {
-  log(message, name: '[oref] warning', level: 900);
-}
--- pub/oref/src/types.dart
+++
@@ -1,6 +0,0 @@
-abstract interface class Ref<T> {
-  T get value;
-  set value(T _);
-}
-
-abstract interface class Derived<T> extends Ref<T> {}
