2d6466ee3a9bd40cf71cf263788fd10f1746b052
Seven Du <shiweidu@outlook.com>
2024-10-09 00:49:01 +0800

    chore: Update example

--- pub/oncecall/example/main.dart
+++ pub/oncecall/example/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:oncecall/oncecall.dart';
 
+void main() {
+  runApp(const App());
+}
+
+int counter = 0;
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final value = oncecall(context, () => ++counter);
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          // It is always "Count: 1" because it is memoized.
+          child: Text('Count: $value'),
+        ),
+      ),
+    );
+  }
+}
