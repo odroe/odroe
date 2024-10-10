950cfff0ecc7608638e96d2049ea7def5dbd70f1
Seven Du <shiweidu@outlook.com>
2024-10-10 16:31:20 +0800

    chore: add og title

--- sites/odroe.dev/.vitepress/config/zh.ts
+++ sites/odroe.dev/.vitepress/config/zh.ts
@@ -4,4 +4,14 @@ export default defineConfig({
   lang: "zh-Hans",
   description:
     "为 Dart 和 Flutter 应用程序中的反应式编程和依赖注入提供了各种实用程序和工具。",
+  head: [
+    [
+      "meta",
+      {
+        property: "og:title",
+        content:
+          "Odroe | 为 Dart 和 Flutter 应用程序中的反应式编程和依赖注入提供了各种实用程序和工具。",
+      },
+    ],
+  ],
 });
