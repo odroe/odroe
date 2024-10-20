cfad9d1e3969a92812688b1dc31d2ba6a391f7d6
Seven Du <shiweidu@outlook.com>
2024-10-20 12:32:51 +0800

    chore: Add `setup` package

---
+++ pub/setup/bin/setup.dart
@@ -0,0 +1,5 @@
+import 'package:setup/setup.dart' as setup;
+
+void main(List<String> arguments) {
+  print('Hello world: ${setup.calculate()}!');
+}
---
+++ pub/setup/test/setup_test.dart
@@ -0,0 +1,8 @@
+import 'package:setup/setup.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
