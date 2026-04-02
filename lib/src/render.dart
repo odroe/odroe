1d2768123706c77c24b02075b38f7480f42adc58
Seven Du <5564821+medz@users.noreply.github.com>
2026-04-03 06:00:04 +0800

    refactor: keep only odroe.dev site assets (#28)

--- lib/src/render.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-extension type const WidgetRender._(Widget Function() _) {
-  Widget call() => _();
-}
-
-WidgetRender h(Widget Function() fn) => WidgetRender._(fn);
